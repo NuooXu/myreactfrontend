dbdf0a5e02d668aad0282b78b9e0d22ae2a021f7
NuooXu <xn19970505@gmail.com>
2020-11-27 15:19:10 -0500

    change app name

--- app/components/Header.js
+++ app/components/Header.js
@@ -6,14 +6,18 @@ import StateContext from "../StateContext"
 
 function Header(props) {
   const appState = useContext(StateContext)
-  const headerContent = appState.loggedIn ? <HeaderLoggedIn /> : <HeaderLoggedOut />
+  const headerContent = appState.loggedIn ? (
+    <HeaderLoggedIn />
+  ) : (
+    <HeaderLoggedOut />
+  )
 
   return (
     <header className="header-bar bg-primary mb-3">
       <div className="container d-flex flex-column flex-md-row align-items-center p-3">
         <h4 className="my-0 mr-md-auto font-weight-normal">
           <Link to="/" className="text-white">
-            ComplexApp
+            Social Media App
           </Link>
         </h4>
         {!props.staticEmpty ? headerContent : ""}
